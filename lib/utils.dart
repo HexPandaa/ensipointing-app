557406c1b5ee7dad838479e5f0b2746a3dc73c7b
HexPandaa <47880094+HexPandaa@users.noreply.github.com>
2021-10-16 16:34:58 +0200

    Implement getPointableCourses and getPointedCourses

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,13 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart';
+import 'package:intl/intl.dart';
+import 'dart:convert' show base64Encode, utf8;
+import 'package:flutter_settings_screens/flutter_settings_screens.dart'
+    show Settings;
+import 'settings.dart';
+import 'courses.dart';
 
 /// From https://api.flutter.dev/flutter/dart-io/HttpClient/findProxyFromEnvironment.html
 /// From https://stackoverflow.com/questions/54763466/flutter-webview-with-proxy
@@ -21,31 +29,140 @@ class ProxyHttpOverrides extends HttpOverrides {
     };
     return client;
   }
-  
-// Alternative
-// @override
-// HttpClient createHttpClient(SecurityContext? context) {
-//   print("Called custom http");
-//   HttpClient client = super.createHttpClient(context);
-//   // HttpClient client = HttpClient(context: context);
-//   client.findProxy = (url) {
-//     print("Called findProxy");
-//     return HttpClient.findProxyFromEnvironment(url, environment: {
-//       "http_proxy": "127.0.0.1:3128",
-//       "https_proxy": "127.0.0.1:3128"
-//     });
-//   };
-//   return client;
-// }
 }
 
-class Courses {
-  static void getCourses() async {
-    HttpOverrides.runWithHttpOverrides(() async {
-      var url = Uri.parse('https://extranet.ensimag.fr/assiduite');
-      var response = await http.get(url);
-      print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
+class CoursesHTTPClient {
+  static Future<List<Course>> getPointableCourses() async {
+    var url = Settings.getValue<String>(AppSettingKeys.platformUrl, "");
+    var response = await _doGet(url);
+    print('Response status: ${response.statusCode}');
+
+    if (response.statusCode == 401) {
+      throw Exception("Bad user credentials");
+    }
+
+    var document = parse(response.body);
+    var adeTable = document.getElementById('adeEvent');
+    if (adeTable == null) {
+      throw Exception("Could not find adeTable");
+    }
+
+    List<Course> courses = [];
+
+    var adeBody = adeTable.getElementsByTagName('tbody');
+    if (adeBody.isEmpty) {
+      print("Not tbody found in adeTable, there must be no course to point");
+      return courses; // empty
+    }
+
+    List<Element> toPointRows = adeBody[0].getElementsByTagName('tr');
+
+    for (Element row in toPointRows) {
+      Course course = _parseTableRow(row);
+      course.setState(CourseState.notYetPointed);
+      print("Adding course " + course.name);
+      courses.add(course);
+    }
+
+    return courses;
+  }
+
+  static Future<List<Course>> getPointedCourses() async {
+    var url = Settings.getValue<String>(AppSettingKeys.platformUrl, "");
+    var response = await _doGet(url);
+    print('Response status: ${response.statusCode}');
+
+    if (response.statusCode == 401) {
+      throw Exception("Bad user credentials");
+    }
+
+    var document = parse(response.body);
+    var tables = document.getElementsByTagName('table');
+    if (tables.length < 3) {
+      throw Exception("Could not find the table of pointed courses");
+    }
+
+    List<Course> courses = [];
+
+    List<Element> toPointRows = tables[2].getElementsByTagName('tr');
+
+    if (toPointRows.length < 2) {
+      // There is only the thead row
+      return courses; // empty
+    }
+
+    toPointRows.removeAt(0); // Remove the thead row
+
+    for (Element row in toPointRows) {
+      Course course = _parseTableRow(row);
+      course.setState(CourseState.pointed);
+      print("Adding course " + course.name);
+      courses.add(course);
+    }
+
+    return courses;
+  }
+
+  static Future<http.Response> _doGet(uri) async {
+    var url = Uri.parse(uri);
+    http.Response response = await HttpOverrides.runWithHttpOverrides(() async {
+      var r = await http.get(url, headers: <String, String>{
+        'Authorization': _getAuthorizationString()
+      });
+      return r;
     }, ProxyHttpOverrides());
+    return response;
+  }
+
+  static String _getAuthorizationString() {
+    // Hardcode for now
+    var user = Settings.getValue<String>(AppSettingKeys.platformUsername, "");
+    var pass = Settings.getValue<String>(AppSettingKeys.platformPassword, "");
+    return 'Basic ' + base64Encode(utf8.encode('$user:$pass'));
+  }
+
+  static Course _parseTableRow(Element row) {
+    String id = row.attributes['ide'].toString();
+    var infos = row.getElementsByTagName('td');
+    // idEvent, activityId, date, hDebut, hFin, name, groupeEtudiant, salle, enseignant
+    int _id = int.parse(infos[0].text);
+    String date = infos[2].text;
+    String hStart = infos[3].text;
+    String hEnd = infos[4].text;
+    String name = infos[5].text;
+    String room = infos[7].text;
+
+    DateFormat format = DateFormat('dd/MM/yyyy hh:mm');
+    DateTime dateStart = format.parse(date + ' ' + hStart);
+    DateTime dateEnd = format.parse(date + ' ' + hEnd);
+
+    return Course(
+        name: name,
+        room: room,
+        timeStart: dateStart,
+        timeEnd: dateEnd,
+        id: _id);
+  }
+
+  static String? allSettingsSet() {
+    if (Settings.getValue(AppSettingKeys.proxyHost, '') == '') {
+      return "Proxy host";
+    } else if (Settings.getValue<String>(AppSettingKeys.proxyUsername, '') ==
+        '') {
+      return "Proxy username";
+    } else if (Settings.getValue<String>(AppSettingKeys.proxyPassword, '') ==
+        '') {
+      return "Proxy password";
+    } else if (Settings.getValue<String>(AppSettingKeys.platformUrl, '') ==
+        '') {
+      return "Platform URL";
+    } else if (Settings.getValue<String>(AppSettingKeys.platformUsername, '') ==
+        '') {
+      return "Platform username";
+    } else if (Settings.getValue<String>(AppSettingKeys.platformPassword, '') ==
+        '') {
+      return "Platform password";
+    }
+    return null;
   }
 }
